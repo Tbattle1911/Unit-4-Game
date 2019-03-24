833bf05555df2304c3763e3e0340bef9f136d1ec
Tbattle1911 <47726339+Tbattle1911@users.noreply.github.com>
2019-03-24 03:21:21 -0400

    Add files via upload
    Assignment 4 updates

---
+++ game.js
@@ -0,0 +1,15 @@
+var wins = "";
+
+var losses= "";
+
+var totalScore= wins - losses;
+
+
+document.querySelector('#title').addEventListener('click', function() {
+    console.log(Math.floor(Math.random() * 50))
+})
+//document.querySelector
+$('#title').on('click', function() {
+    
+})
+
